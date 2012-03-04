89af8f745b90c1ce23037236d0d89cbc62330487
Xavier Basty <xbasty@gmail.com>
2012-03-04 11:39:59 +0100

    Removed dependencies to jquery.colorhelpers

--- flotR.js
+++ flotR.js
@@ -3,11 +3,180 @@
  * Released under the MIT license by Xavier Basty, March 2012.
  *
  * Based on the flot plotting library (http://code.google.com/p/flot/)
+ * Color management based on jquery.colorhelpers by Ole Laursen
  *
  */
 
 (function ($, undefined) {
   "use strict";
+  var R = Raphael;
+
+  function Color (r, g, b, a) {
+    if (this instanceof Color) {
+      this.r = r || 0;
+      this.g = g || 0;
+      this.b = b || 0;
+      this.a = a != null ? a : 1;
+      return this.normalize();
+    } else {
+      return new Color(r, g, b, a);
+    }
+  }
+
+  Color.prototype = {
+    constructor: Color,
+    scale: function (c, f) {
+      var ci, cl;
+      for (ci = 0, cl = c.length; ci < cl; ci++) {
+        this[c.charAt(ci)] *= f;
+      }
+      return this.normalize();
+    },
+    normalize: function () {
+      var c = this;
+      function clamp(min, value, max) {
+        return value < min ? min: (value > max ? max: value);
+      }
+
+      c.r = clamp(0, parseInt(c.r, 10), 255);
+      c.g = clamp(0, parseInt(c.g, 10), 255);
+      c.b = clamp(0, parseInt(c.b, 10), 255);
+      c.a = clamp(0, c.a, 1);
+
+      return c;
+    },
+    toString: function () {
+      var c = this;
+      if (c.a >= 1.0) {
+        return "rgb("+[c.r, c.g, c.b].join(",")+")";
+      } else {
+        return "rgba("+[c.r, c.g, c.b, c.a].join(",")+")";
+      }
+    }
+  };
+
+  Color.LookupColors = {
+    aqua:[0,255,255],
+    azure:[240,255,255],
+    beige:[245,245,220],
+    black:[0,0,0],
+    blue:[0,0,255],
+    brown:[165,42,42],
+    cyan:[0,255,255],
+    darkblue:[0,0,139],
+    darkcyan:[0,139,139],
+    darkgrey:[169,169,169],
+    darkgreen:[0,100,0],
+    darkkhaki:[189,183,107],
+    darkmagenta:[139,0,139],
+    darkolivegreen:[85,107,47],
+    darkorange:[255,140,0],
+    darkorchid:[153,50,204],
+    darkred:[139,0,0],
+    darksalmon:[233,150,122],
+    darkviolet:[148,0,211],
+    fuchsia:[255,0,255],
+    gold:[255,215,0],
+    green:[0,128,0],
+    indigo:[75,0,130],
+    khaki:[240,230,140],
+    lightblue:[173,216,230],
+    lightcyan:[224,255,255],
+    lightgreen:[144,238,144],
+    lightgrey:[211,211,211],
+    lightpink:[255,182,193],
+    lightyellow:[255,255,224],
+    lime:[0,255,0],
+    magenta:[255,0,255],
+    maroon:[128,0,0],
+    navy:[0,0,128],
+    olive:[128,128,0],
+    orange:[255,165,0],
+    pink:[255,192,203],
+    purple:[128,0,128],
+    violet:[128,0,128],
+    red:[255,0,0],
+    silver:[192,192,192],
+    white:[255,255,255],
+    yellow:[255,255,0]
+  };
+
+  /**
+   * Parse CSS color string (like "rgb(10, 32, 43)" or "#fff")
+   * @param {!String} str
+   * @return {!Color} The {@link Color} object representing the parsed string
+   */
+  Color.parse = function (str) {
+    var res, name, c = Color;
+
+    // Look for rgb(num,num,num)
+    if (res = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(str)) {
+      return c(parseInt(res[1], 10), parseInt(res[2], 10), parseInt(res[3], 10));
+    }
+
+    // Look for rgba(num,num,num,num)
+    if (res = /rgba\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]+(?:\.[0-9]+)?)\s*\)/.exec(str)) {
+      return c(parseInt(res[1], 10), parseInt(res[2], 10), parseInt(res[3], 10), parseFloat(res[4]));
+    }
+
+    // Look for rgb(num%,num%,num%)
+    if (res = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(str)) {
+      return c(parseFloat(res[1]) * 2.55, parseFloat(res[2]) * 2.55, parseFloat(res[3]) * 2.55);
+    }
+
+    // Look for rgba(num%,num%,num%,num)
+    if (res = /rgba\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\s*\)/.exec(str)) {
+      return c(parseFloat(res[1]) * 2.55, parseFloat(res[2]) * 2.55, parseFloat(res[3]) * 2.55, parseFloat(res[4]));
+    }
+
+    // Look for #a0b1c2
+    if (res = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(str)) {
+      return c(parseInt(res[1], 16), parseInt(res[2], 16), parseInt(res[3], 16));
+    }
+
+    // Look for #fff
+    if (res = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(str)) {
+      return c(parseInt(res[1] + res[1], 16), parseInt(res[2] + res[2], 16), parseInt(res[3] + res[3], 16));
+    }
+
+    // Otherwise, we're most likely dealing with a named color
+    name = str.replace(/^\s+/, '').replace(/\s+$/, '').toLowerCase();
+    if (name == 'transparent') {
+      return c(255, 255, 255, 0);
+    }
+    else {
+      res = Color.LookupColors[name];
+      return c(res[0], res[1], res[2]);
+    }
+  };
+
+  /**
+   * Extract CSS color property from element, going up in the DOM if it's "transparent"
+   * TODO: Remove jQuery dependency
+   *
+   * @param elem
+   * @param css
+   * @return {!Color}
+   */
+  Color.extract = function (elem, css) {
+    var c;
+    do {
+      c = elem.css(css).toLowerCase();
+
+      // keep going until we find an element that has color, or we hit the body
+      if (c != '' && c != 'transparent') {
+        break;
+      }
+      elem = elem.parent();
+    } while (!jQuery.nodeName(elem.get(0), 'body'));
+
+    // catch Safari's way of signalling transparent
+    if (c == 'rgba(0, 0, 0, 0)') {
+      c = 'transparent';
+    }
+
+    return Color.parse(c);
+  };
 
   /**
    * Round to nearby lower multiple of base
@@ -63,8 +232,8 @@
 
   function onHoverIn () {
     var
-      strokeStyle = $.color.parse(this.attrs.stroke).scale('a', 0.5).toString(),
-      fillStyle = $.color.parse(this.attrs.fill).scale('a', 0.5).toString();
+      strokeStyle = Color.parse(this.attrs.stroke).scale('a', 0.5).toString(),
+      fillStyle = Color.parse(this.attrs.fill).scale('a', 0.5).toString();
 
     this.attr({
       stroke: strokeStyle,
@@ -74,8 +243,8 @@
 
   function onHoverOut () {
     var
-      strokeStyle = $.color.parse(this.attrs.stroke).scale('a', 2).toString(),
-      fillStyle = $.color.parse(this.attrs.fill).scale('a', 2).toString();
+      strokeStyle = Color.parse(this.attrs.stroke).scale('a', 2).toString(),
+      fillStyle = Color.parse(this.attrs.fill).scale('a', 2).toString();
 
     this.attr({
       stroke: strokeStyle,
@@ -180,7 +349,7 @@
           assignedColors.push(sc);
         }
         else {
-          usedColors.push($.color.parse(series[i].color));
+          usedColors.push(Color.parse(series[i].color));
         }
       }
     }
@@ -195,9 +364,9 @@
     l = options.colors.length;
     while (colors.length < neededColors) {
       if (l === i) {  // check degenerate case
-        c = $.color.make(100, 100, 100);
+        c = Color(100, 100, 100);
       } else {
-        c = $.color.parse(options.colors[i]);
+        c = Color.parse(options.colors[i]);
       }
 
       // vary color if needed
@@ -1236,8 +1405,8 @@
         if (color == null) {
           color = options.grid.backgroundColor;
           color = color && typeof color === 'string'
-            ? $.color.parse(color)
-            : $.color.extract(legend, 'background-color');
+            ? Color.parse(color)
+            : Color.extract(legend, 'background-color');
           color.a = 1;
           color = color.toString();
         }
@@ -1265,7 +1434,7 @@
       c = spec.colors[ci];
 
       if (typeof c !== 'string') {
-        co = $.color.parse(defaultColor);
+        co = Color.parse(defaultColor);
         if (c.brightness != null) {
           co = co.scale('rgb', c.brightness);
         }
@@ -1298,7 +1467,7 @@
       return getColorOrGradient(fillOptions.fillColor, seriesColor);
     }
 
-    color = $.color.parse(seriesColor);
+    color = Color.parse(seriesColor);
     color.a = typeof fill == 'number' ? fill : 0.4;
     color.normalize();
     return color.toString();
@@ -1480,7 +1649,7 @@
           : box.left - plotr.plotOffset.left + (axis.position == "left" ? box.width : 0);
       }
 
-      color = axis.options.tickColor || $.color.parse(axis.options.color).scale('a', 0.22).toString();
+      color = axis.options.tickColor || Color.parse(axis.options.color).scale('a', 0.22).toString();
 
       // Draw tick bar
       if (!axis.innermost) {
@@ -2470,7 +2639,7 @@
       options.yaxis.tickColor = options.yaxis.tickColor || options.grid.tickColor;
 
       options.grid.borderColor = options.grid.borderColor || options.grid.color;
-      options.grid.tickColor = options.grid.tickColor || $.color.parse(options.grid.color).scale('a', 0.22).toString();
+      options.grid.tickColor = options.grid.tickColor || Color.parse(options.grid.color).scale('a', 0.22).toString();
 
       // Fill in defaults in axes,
       // copy at least always the first as the rest of the code assumes it'll be there
@@ -2536,7 +2705,7 @@
       this.placeholder = placeholder = $(placeholder);
       this.canvasWidth = placeholder.width();
       this.canvasHeight = placeholder.height();
-      this.canvas = Raphael(placeholder.get(0), this.canvasWidth, this.canvasHeight);
+      this.canvas = R(placeholder.get(0), this.canvasWidth, this.canvasHeight);
 
       //TODO: Handle reuse?
     },
